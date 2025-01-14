c72997c6dd74725bedb868d7aece4de7d4a4858d
Kristiqn Stoichev <stu2301261014@uni-plovdiv.bg>
2025-01-14 17:56:42 +0200

    Animations

---
+++ AdventureTime/Uni/Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+    
+
+public class ItemCollector : MonoBehaviour
+{
+  private int Pineapples = 0;
+  [SerializeField] private Text PineapplesText;
+
+  [SerializeField] private AudioSource collectionSoundEffect;
+
+  private void OnTriggerEnter2D(Collider2D collision)
+  {
+    if (collision.gameObject.CompareTag("Pineapple"))
+    {
+        collectionSoundEffect.Play();
+        Destroy(collision.gameObject);
+        Pineapples++;
+        PineapplesText.text = "Pineapples : " + Pineapples;
+        
+    }
+  }
+}
